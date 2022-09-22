40c9434cfc890039f7b3d5fb662e82ade26aeea6
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-09-22 16:19:08 +0200

    first commit

---
+++ app/Models/Post.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    //
+    protected $fillable = [
+        'title',
+        'user_id',
+        'post_content',
+        'post_image',
+        'post_date'
+    ];
+
+    public function user(){
+        return $this->belongsTo('App\User');
+    }
+}
