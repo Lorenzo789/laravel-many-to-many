302db79e3db206c5b2ba99fb6d973e091aff807a
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-09-22 17:57:40 +0200

    exercise done

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Post;
+use App\Models\Tag;
 use DateTime;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -11,6 +11,15 @@
                     <h3 class="card-title">{{ $post->title }}</h3>
                     <p class="card-text">{{ $post->post_content }}</p>
                     <pre>{{ $post->user->name }} | {{ $post->post_date }}</pre>
+                    <span class="badge badge-pill">
+                        @if ( isset($post->tags) )
+                            @foreach ($post->tags as $tag)
+                                {{ $tag->name }} -
+                            @endforeach
+                        @else
+                            no tag selected for this post
+                        @endif
+                    </span>
                 </div>
             </div>
         </div>
