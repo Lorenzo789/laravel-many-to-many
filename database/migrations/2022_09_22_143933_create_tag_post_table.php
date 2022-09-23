218a5922a4e9219b6f23815392a8e6bb76ae2f9d
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-09-23 18:04:11 +0200

    on delete done

--- database/migrations/2022_09_22_143933_create_tag_post_table.php
+++ database/migrations/2022_09_22_143933_create_tag_post_table.php
@@ -17,12 +17,14 @@ class CreateTagPostTable extends Migration
             $table->unsignedBigInteger('tag_id');
                 $table->foreign('tag_id')
                 ->references('id')
-                ->on('tags');
+                ->on('tags')
+                ->onDelete('cascade');
 
             $table->unsignedBigInteger('post_id');
                 $table->foreign('post_id')
                 ->references('id')
-                ->on('posts');
+                ->on('posts')
+                ->onDelete('cascade');
 
             $table->primary(['post_id', 'tag_id']);
 
