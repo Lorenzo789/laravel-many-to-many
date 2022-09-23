8e8145963c1eb348089563a6a41a4aa531b824d0
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-09-23 17:04:05 +0200

    add button to tags index

--- resources/views/admin/tags/index.blade.php
+++ resources/views/admin/tags/index.blade.php
@@ -21,9 +21,24 @@
                                 {{ $tag->name }}
                             </a>
                         </td>
+                        <td class="text-right">
+                            <a href="{{ route('admin.tags.edit', $tag->id) }}" class="btn btn-success">
+                                Edit
+                            </a>
+                            <form action="{{ route('admin.tags.destroy', $tag->id) }}" method="POST" class="d-inline px-2">
+                                @method('DELETE')
+                                @csrf
+                                <button type="submit" class="btn btn-danger">
+                                    Delete
+                                </button>
+                            </form>
+                            <a class="btn btn-primary" href="{{ route('admin.tags.create') }}">
+                                Add a tag
+                            </a>
+                        </td>
                     </tr>
                 @empty
-                    
+                    there are no tag
                 @endforelse
             </tbody>
         </table>
